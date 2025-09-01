7ea089b17ea449d922d7664eed716c2488307ab7
Christopher Ivy <CHRISIVY22703@GMAIL.COM>
2025-09-01 16:31:04 -0500

    Corrected some backend code to include AWS CA in order to connect to the back end.

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,16 +1,24 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv")
+const fs = require("fs");
+const path = require("path");
+const caFilePath = path.join(__dirname, "global-bundle.pem"); 
 
 dotenv.config()
 
 
 //Connect to DB
 mongoose.connect
-    ("mongodb://PortfolioDBMaster:DemonJoker12!@portfolio-site-db.cluster-ctg8g4swosxc.us-east-2.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false")
-    .then(() => console.log(`AWS DB Connected`))
-    .catch((err) =>
-        console.log(`Something went wrong when connecting to AWS DB`, err),
-        function (client) {
-            //Specify the database to be used
-            db = client.db('portfolio-site-db')
-        });
+    (MongoDB_Connection_Point, {
+        ssl: true,
+        sslCA: fs.readFileSync(caFilePath),
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }),
+    function (client) {
+        //Specify the database to be used
+        db = client.db('portfolio-site-db')
+    }
+        .then(() => console.log(`AWS DB Connected`))
+        .catch((err) =>
+            console.log(`Something went wrong when connecting to AWS DB`, err));
--- server/server.js
+++ server/server.js
@@ -3,12 +3,12 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config()
 const app = express();
-const port = process.env.PORT
+const port = process.env.BACKEND_PORT
 const ProjectRoutes = require("./routes/project.routes");
 
 require('./config/mongoose.config');
 
-app.use(cors({ credentials: true, origin: 'http://localhost:5173' }))
+app.use(cors({ credentials: true, origin: 'http://18.116.33.192' }))
 app.use(express.json(), express.urlencoded({ extended: true }));
 
 ProjectRoutes(app);
